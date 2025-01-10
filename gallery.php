b37f42b536515a5b473e29b990ef911ad0ba676b
PriyaT <thangamanithangamani7775@.com>
2025-01-10 22:52:56 +0530

    added index file

---
+++ gallery.php
@@ -0,0 +1,230 @@
+<html>
+		<head>
+		    <meta name="viewport" content="width=device-width, initial-scale=1">
+           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		<style>
+		
+		.topnav {
+	  overflow: hidden;
+	  background-color: #566;
+	}
+
+	.topnav a {
+	  float: left;
+	  color: #f2f2f2;
+	  text-align: center;
+	  padding: 14px 16px;
+	  text-decoration: none;
+	  font-size: 17px;
+	}
+
+	.topnav a:hover {
+	  background-color: #ddd;
+	  color: black;
+	}
+
+	.topnav a.active {
+	  background-color: #04AA6D;
+	  color: white;
+	}
+	
+	* {
+  box-sizing: border-box;
+}
+
+.column {
+  float: left;
+  width: 33.33%;
+  padding: 5px;
+}
+
+/* Clearfix (clear floats) */
+.row::after {
+  content: "";
+  clear: both;
+  display: table;
+}
+
+    .dropdown {
+  float: left;
+  overflow: hidden;
+}
+
+.dropdown .dropbtn {
+  font-size: 16px;  
+  border: none;
+  outline: none;
+  color: white;
+  padding: 14px 16px;
+  background-color: inherit;
+  font-family: inherit;
+  margin: 0;
+}
+
+.navbar a:hover, .dropdown:hover .dropbtn {
+  background-color: red;
+}
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #f9f9f9;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+}
+
+.dropdown-content a {
+  float: none;
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+  text-align: left;
+}
+
+.dropdown-content a:hover {
+  background-color: #ddd;
+}
+
+.dropdown:hover .dropdown-content {
+  display: block;
+}
+
+
+.dropdown {
+  float: left;
+  overflow: hidden;
+}
+
+.dropdown .dropbtn {
+  font-size: 16px;  
+  border: none;
+  outline: none;
+  color: white;
+  padding: 14px 16px;
+  background-color: inherit;
+  font-family: inherit;
+  margin: 0;
+}
+
+.navbar a:hover, .dropdown:hover .dropbtn {
+  background-color: red;
+}
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #f9f9f9;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+}
+
+.dropdown-content a {
+  float: none;
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+  text-align: left;
+}
+
+.dropdown-content a:hover {
+  background-color: #ddd;
+}
+
+.dropdown:hover .dropdown-content {
+  display: block;
+}
+		</style>
+		</head>
+		<body bgcolor="white">
+        <h2 style="color:rgba(16, 17, 17, 0.822);font-size:25px;">
+            <center>
+            <marquee width="30%" direction="right" height="50px">
+          KATTABOMMAN HOTEL</marquee></center>
+            </h2>
+            <div class="topnav">
+                <a class="active" href="main.html">Home</a>
+                <div class="dropdown" href="#hospitalfacilty">
+                    <button class="dropbtn">Menu</button> 
+                   <div class="dropdown-content">
+                    
+                         <a href="stack1.html">Stack Quantity</a>
+                     <a href="item1.html">Items</a>
+                     <a href="packing.html">Packing Items</a>
+                     <a href="#gallery.php">Other Items</a>
+                     
+                   </div></div>
+                   <a href="gallery.php">Gallery</a>
+                <a href="contactus.html">Contact</a>
+            </div>
+            
+    </body>
+</html>
+	   
+	   <div class="row">
+  <div class="column">
+    <img src="hotelimg.jpg" style="width:100%">
+  </div>
+  <div class="column">
+    <img src="bio.jpeg"  style="width:100%">
+  </div>
+  <div class="column">
+    <img src="dhosai.jpeg" style="width:100%">
+  </div>
+</div>
+
+<div class="row">
+  <div class="column">
+    <img src="large.jpg"  style="width:100%">
+  </div>
+  <div class="column">
+    <img src="idli.jpg"  style="width:100%">
+  </div>
+  <div class="column">
+    <img src="OLC.jpeg" style="width:100%">
+  </div>
+</div>
+
+<div class="row">
+  <div class="column">
+    <img src="spil.jpeg"  style="width:100%">
+  </div>
+  <div class="column">
+    <img src="spl.jpg"  style="width:100%">
+  </div>
+  <div class="column">
+    <img src="th.jpeg" alt="Mountains" style="width:100%">
+  </div>
+</div>
+
+<div class="row">
+  <div class="column">
+    <img src="menuitem.jpg"  style="width:100%">
+  </div>
+  <div class="column">
+    <img src="tomato.jpg" style="width:100%">
+  </div>
+  <div class="column">
+    <img src="tea.jpeg"  style="width:100%">
+  </div>
+</div>
+
+<div class="row">
+  <div class="column">
+    <img src="outside.jpg"  style="width:100%">
+  </div>
+  <div class="column">
+    <img src="cool.jpeg"  style="width:100%">
+  </div>
+  <div class="column">
+    <img src="coolimg.jpeg"  style="width:100%">
+  </div>
+</div>
+
+
+
+
+		   </body>
